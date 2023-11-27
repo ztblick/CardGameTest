17d54b0a986bad2f2fcd01998610209ef14c7e37
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2023-11-27 09:45:03 -0800

    Updated Card class with instance variables.

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    private String suit;
+    private int value;
+
+    public Card()
+    {
+        suit = "Diamonds";
+        value = 3;
+    }
 }
